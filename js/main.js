373938f9684c46fced94c0b6d192d3600668c24e
Ahmed Helal <87047003+HELAL15@users.noreply.github.com>
2023-09-19 20:15:41 +0300

    contact us page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 
 
+/* navbar */
+
+let bars = document.querySelector('.open');
+let close = document.querySelector('.close');
+
+if(bars != null && close != null){
+  bars.addEventListener('click' , ()=>{
+    bars.style.display = 'none';
+    close.style.display = 'block';
+  })
+  close.addEventListener('click' , ()=>{
+    bars.style.display = 'block';
+    close.style.display = 'none';
+  })
+
+
+  function showBG() {
+    let scroll = window.scrollY;
+    let isNavTop = scroll < 200;
+    // console.log(scroll)
+    let nav = document.getElementById('nav');
+    // Compare the scroll position to 50
+    if ( isNavTop) {
+      nav.classList.remove('bgShow');
+    } else {
+      nav.classList.add('bgShow');
+    }
+  }
+  window.addEventListener('scroll', showBG);
+
+
+}
+
+
+
+
 
 const Body = document.getElementsByTagName('body')[0];
 let rtlValue = false;
-if (Body.style.direction = 'rlt') {
+if (Body.style.direction = 'rtl') {
   rtlValue = true;
 }
 
@@ -13,8 +49,8 @@ $('.owl-carousel').owlCarousel({
   margin:1,
   nav:true,
   dots: false,
-  autoplay:false,
-  autoplayTimeout:5000,
+  autoplay:true,
+  autoplayTimeout:2500,
   responsive:{
       0:{
           items:1
